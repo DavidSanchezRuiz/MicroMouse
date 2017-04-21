82985a6e5c9eb392fdd2ff41c29c817135386f26
DavidSanchezRuiz <davidignacio.sanchez@uptc.edu.co>
2017-04-20 20:37:44 -0500

    añadida .h para controlar motores, aun no se ha probado

---
+++ BridgeH.h
@@ -0,0 +1,45 @@
+class BridgeH{
+private:
+  int in1this,in2this,in3this,in4this;
+public:
+  BridgeH(int in1, int in2, int in3, int in4);
+  void fordward();
+  void reverse();
+  void left();
+  void right();
+};
+
+BridgeH::BridgeH(int in1, int in2, int in3, int in4){
+  in1this=in1;
+  in2this=in2;
+  in3this=in3;
+  in4this=in4;
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+}
+void BridgeH::fordward(){
+  digitalWrite(in1this, HIGH);
+  digitalWrite(in2this, LOW);
+  digitalWrite(in3this, HIGH);
+  digitalWrite(in4this, LOW);
+}
+void BridgeH::reverse(){
+  digitalWrite(in1this, LOW);
+  digitalWrite(in2this, HIGH);
+  digitalWrite(in3this, LOW);
+  digitalWrite(in4this, HIGH);
+}
+void BridgeH::left(){
+  digitalWrite(in1this, HIGH);
+  digitalWrite(in2this, LOW);
+  digitalWrite(in3this, LOW);
+  digitalWrite(in4this, HIGH);
+}
+void BridgeH::right(){
+  digitalWrite(in1this, LOW);
+  digitalWrite(in2this, HIGH);
+  digitalWrite(in3this, HIGH);
+  digitalWrite(in4this, LOW);
+}
---
+++ MicroMouse.ino
@@ -0,0 +1,9 @@
+#include "BridgeH.h"
+BridgeH bh(9,10,14,15);
+void setup() {
+  
+}
+
+void loop() {
+  bh.right();
+}
