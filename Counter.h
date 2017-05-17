030afce4e9ed14a1767aaf249edd8f82dfdeed3f
DavidSanchezRuiz <davidignacio.sanchez@uptc.edu.co>
2017-05-16 23:07:48 -0500

    Contador de pasos

---
+++ Counter.h
@@ -0,0 +1,25 @@
+class Counter{
+private:
+  int count,state,pin;
+public:
+  Counter(int pin);
+  int getCount();
+  void sum();
+};
+
+Counter::Counter(int pinT){
+  count=0;
+  pinMode(pin,INPUT);
+  pin=pinT;
+  state=digitalRead(pin);
+}
+
+int Counter::getCount(){
+  return count;
+}
+void Counter::sum(){
+  if (!digitalRead(pin)==state){
+    state=digitalRead(pin);
+    count++;
+  }
+}
--- MicroMouse.ino
+++ MicroMouse.ino
@@ -1,8 +1,17 @@
 #include "BridgeH.h"
+#include "Counter.h"
 BridgeH bh(5,6,9,10);//1-4
+Counter c(2);
 void setup() {
+  interrupts();
+  pinMode(2,INPUT);
+  pinMode(3,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), count, RISING);
 }
 void loop() {
   bh.fordward();
+}
+void count(){
+  c.sum();
 }
 
