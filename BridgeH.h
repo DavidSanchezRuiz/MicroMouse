1451bc766ca319ec9998035c78c2bbf4b726fae6
franciscobarrerarodriguez <francisco.barrera@uptc.edu.co>
2017-06-11 17:13:49 -0500

    Agregados comentarios

--- BridgeH.h
+++ BridgeH.h
@@ -1,7 +1,14 @@
-class BridgeH{
+/*
+* Controla las acciones de un puente H.
+*/
+class BridgeH {
+
 private:
-  int in1this,in2this,in3this,in4this;
+
+  int in1this, in2this, in3this, in4this;
+
 public:
+
   BridgeH(int in1, int in2, int in3, int in4);
   void stopH();
   void fordward();
@@ -9,42 +16,63 @@ public:
   void left();
   void right();
 };
-
-BridgeH::BridgeH(int in1, int in2, int in3, int in4){
-  in1this=in1;
-  in2this=in2;
-  in3this=in3;
-  in4this=in4;
-  pinMode(in1,OUTPUT);
-  pinMode(in2,OUTPUT);
-  pinMode(in3,OUTPUT);
-  pinMode(in4,OUTPUT);
+/*
+* Recibe como parametros las 4 salidas del puente H.
+*/
+BridgeH::BridgeH(int in1, int in2, int in3, int in4) {
+  in1this = in1;
+  in2this = in2;
+  in3this = in3;
+  in4this = in4;
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
 }
-void BridgeH::stopH(){
+
+/*
+* Cambia el estado de todas las salidas del puente H a LOW, detine el mouse.
+*/
+void BridgeH::stopH() {
   digitalWrite(in1this, LOW);
   digitalWrite(in2this, LOW);
   digitalWrite(in3this, LOW);
   digitalWrite(in4this, LOW);
 }
-void BridgeH::fordward(){
+/*
+* Cambia el estado de las salidas de forma que el mouse vaya hacia adelante.
+*/
+void BridgeH::fordward() {
   digitalWrite(in1this, HIGH);
   digitalWrite(in2this, LOW);
   digitalWrite(in3this, HIGH);
   digitalWrite(in4this, LOW);
 }
-void BridgeH::reverse(){
+
+/*
+* Cambia el estado de las salidas de forma que el mouse vaya hacia atras.
+*/
+void BridgeH::reverse() {
   digitalWrite(in1this, LOW);
   digitalWrite(in2this, HIGH);
   digitalWrite(in3this, LOW);
   digitalWrite(in4this, HIGH);
 }
-void BridgeH::left(){
+
+/*
+* Cambia el estado de las salidas de forma que el mouse vaya hacia la izquierda.
+*/
+void BridgeH::left() {
   digitalWrite(in1this, HIGH);
   digitalWrite(in2this, LOW);
   digitalWrite(in3this, LOW);
   digitalWrite(in4this, HIGH);
 }
-void BridgeH::right(){
+
+/*
+* Cambia el estado de las salidas de forma que el mouse vaya hacia la derecha.
+*/
+void BridgeH::right() {
   digitalWrite(in1this, LOW);
   digitalWrite(in2this, HIGH);
   digitalWrite(in3this, HIGH);
--- MicroMouse.ino
+++ MicroMouse.ino
@@ -1,9 +1,13 @@
 #include "BridgeH.h"
 #include "Counter.h"
 #include "Ultra.h"
+
 BridgeH bh(5,6,9,10);//1-4
+
 Counter c(2);
+
 Ultra u(11,12);
+
 void setup() {
   //interrupts();
   //attachInterrupt(digitalPinToInterrupt(2), count, RISING);
@@ -13,12 +17,13 @@ void setup() {
 void loop() {
   Serial.println(u.getD());
   //bh.right();
-  if(u.getD()>4){
+  if(u.getD() > 4){
     bh.fordward();
   }else{
     bh.stopH();
   }
 }
+
 void count(){
   c.sum();
 }
