b55a59206a0d1413fbe734c4b32d3ba19c2587a8
DavidSanchezRuiz <davidignacio.sanchez@uptc.edu.co>
2017-06-14 22:50:49 -0500

    in modificados por this

--- BridgeH.h
+++ BridgeH.h
@@ -7,7 +7,7 @@ class BridgeH {
 
 private:
 
-  int in1this, in2this, in3this, in4this;
+  int in1, in2, in3, in4;
 
 public:
 
@@ -22,10 +22,10 @@ public:
 * Recibe como parametros las 4 salidas del puente H.
 */
 BridgeH::BridgeH(int in1, int in2, int in3, int in4) {
-  in1this = in1;
-  in2this = in2;
-  in3this = in3;
-  in4this = in4;
+  this->in1= in1;
+  this->in2= in2;
+  this->in3= in3;
+  this->in4= in4;
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
@@ -36,48 +36,48 @@ BridgeH::BridgeH(int in1, int in2, int in3, int in4) {
 * Cambia el estado de todas las salidas del puente H a LOW, detine el mouse.
 */
 void BridgeH::stopH() {
-  digitalWrite(in1this, LOW);
-  digitalWrite(in2this, LOW);
-  digitalWrite(in3this, LOW);
-  digitalWrite(in4this, LOW);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 /*
 * Cambia el estado de las salidas de forma que el mouse vaya hacia adelante.
 */
 void BridgeH::fordward() {
-  digitalWrite(in1this, HIGH);
-  digitalWrite(in2this, LOW);
-  digitalWrite(in3this, HIGH);
-  digitalWrite(in4this, LOW);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
 }
 
 /*
 * Cambia el estado de las salidas de forma que el mouse vaya hacia atras.
 */
 void BridgeH::reverse() {
-  digitalWrite(in1this, LOW);
-  digitalWrite(in2this, HIGH);
-  digitalWrite(in3this, LOW);
-  digitalWrite(in4this, HIGH);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
 
 /*
 * Cambia el estado de las salidas de forma que el mouse vaya hacia la izquierda.
 */
 void BridgeH::left() {
-  digitalWrite(in1this, HIGH);
-  digitalWrite(in2this, LOW);
-  digitalWrite(in3this, LOW);
-  digitalWrite(in4this, HIGH);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
 
 /*
 * Cambia el estado de las salidas de forma que el mouse vaya hacia la derecha.
 */
 void BridgeH::right() {
-  digitalWrite(in1this, LOW);
-  digitalWrite(in2this, HIGH);
-  digitalWrite(in3this, HIGH);
-  digitalWrite(in4this, LOW);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
 }
 #endif /* BRIDGE_H */
